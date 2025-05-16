4a5d518887bc2d89635d301120263e6d4a749cbc
Gregory <grigoriikopp@gmail.com>
2025-05-17 00:13:33 +0200

    change age

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # practice
 learning
+and
--- storytell.py
+++ storytell.py
@@ -3,7 +3,7 @@ import pandas as pd
 # Примерные данные
 data = {
     'Name': ['Alice', 'Bob', 'Charlie', 'David'],
-    'Age': [25, 30, 35, 40],
+    'Age': [25, 30, 33, 40],
     'Score': [85.5, 90.3, 78.9, 92.1]
 }
 
